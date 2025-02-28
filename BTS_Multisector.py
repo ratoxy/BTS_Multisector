f314a3774f82b18d70d5723eb966c9a94c4b75ea
ratoxy <ratoxy@hotmail.com>
2025-02-28 19:56:35 +0000

    Update BTS_Multisector.py

--- BTS_Multisector.py
+++ BTS_Multisector.py
@@ -43,8 +43,9 @@ def gerar_grelha(area_coberta, espaco=0.0045):
 
 def main():
     st.set_page_config(layout="wide")
+    st.sidebar.subheader("**Multi Cell View**:blue[**_©2025 NAIIC CTer Santarém_**]")
     st.sidebar.subheader("Configuração do Mapa")
-    st.markdown(":blue[**_©2025 NAIIC CTer Santarém_**]")
+
 
     cores = ["blue", "red", "green"]
     
